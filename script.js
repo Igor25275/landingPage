b43b12ace4e91f82b97a4aacb6860c351d456782
Igor Mariano <santos.98@outlook.com>
2026-03-12 12:31:26 -0300

    adicionando funcoes novos ,  e ais interatividade com o usuario por meio dos botoes

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const mobile_btn = document.querySelector(".btn_mobile");
 const nav_links = document.getElementById("nav_links");
 
+const btn_box = document.querySelector(".btn_box");
+const box_contact = document.getElementById("box_contact");
+
+const btn_close = document.getElementById("btn_close");
+
+btn_close.addEventListener("click", () =>{
+
+    box_contact.classList.remove("show");
+    box_contact.style.display = "none";
+
+})
+
+btn_box.addEventListener("click", () =>{
+
+    box_contact.classList.add("show");
+    box_contact.style.display = "block";
+
+})
+
+
 mobile_btn.addEventListener("click", () => {
 
     nav_links.classList.toggle("show");
